8dc72d6ce538be2fd12e98fffefea90056bea002
KiranJungGurung <kirangurung394@gmail.com>
2022-12-21 17:24:50 +0545

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kiran Gurung on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
